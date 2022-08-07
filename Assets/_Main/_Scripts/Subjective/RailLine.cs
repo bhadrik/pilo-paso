246442570bb2cb1fbdf489bd475bb6a13c28dcb2
bhadrik <jagadbhadrik4@gmail.com>
2022-08-07 12:08:31 +0530

    Working
    Working on rail line graphics done

---
+++ Assets/_Main/_Scripts/Subjective/RailLine.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailLine : MonoBehaviour
+{
+    #region  Variable
+    //------------------------------------//
+
+    //------------------------------------//
+    #endregion
+
+
+
+
+    #region  Unity Method
+    //------------------------------------//
+
+    
+
+    //------------------------------------//
+    #endregion
+
+
+
+
+    #region  Public
+    //------------------------------------//
+    
+    
+
+    //------------------------------------//
+    #endregion
+
+
+
+
+    #region  Private
+    //------------------------------------//
+    
+    
+
+    //------------------------------------//
+    #endregion
+    
+}
